3f0bac0be0cc267fc2bf254d7520feea87e9cf99
misterzurg <imenbearpigi@gmail.com>
2020-09-23 21:41:05 +0300

    Reasonable var names and Project.Name.Splited.by.dots.csproj support.

--- postInstall.fsx
+++ postInstall.fsx
@@ -12,24 +12,23 @@ let getProjectName: string =
     let getDirectoryPath = Directory.GetCurrentDirectory()
 
     let splitedPath =
-        Directory.GetFiles(getDirectoryPath, "*csproj").[0].Split('\\')
+        Directory.GetFiles(getDirectoryPath, "*csproj").[0].Split('\\', '/')
 
-    let fileNameArr =
-        splitedPath.[splitedPath.Length - 1].Split '.'
+    let onlyProjectName =
+        splitedPath.[splitedPath.Length - 1].Replace(".csproj", "")
 
-    fileNameArr.[0]
+    onlyProjectName.Replace(".", "")
 
 for file in dockerFiles do
-    let mutable newName = getProjectName
-    let mutable newRep = replacement
+    let mutable project = getProjectName
+    let mutable template = replacement
     if file.Contains("yml") || file.Contains("dcproj") then
-        newName <- newName.ToLower()
-        newRep <- newRep.ToLower()
+        project <- project.ToLower()
+        template <- template.ToLower()
     let linesArray = ResizeArray<string>()
     for line in File.ReadAllLines(file) do
-        if line.Contains(newRep) then
-            let newText = line.Replace(newRep, newName)
-            linesArray.Add newText
+        if line.Contains(template) then
+            line.Replace(template, project) |> linesArray.Add
         else
             linesArray.Add line
     File.WriteAllLines(file, linesArray)
