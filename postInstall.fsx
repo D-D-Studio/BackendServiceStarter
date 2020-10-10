32e231965797b407c2efa6b3639d26d48ee84aa6
d.vasilyev <d.vasilyev@alfaleads.net>
2020-10-11 01:01:03 +0300

    Refactor

--- postInstall.fsx
+++ postInstall.fsx
@@ -10,22 +10,23 @@ let replacement = "BackendServiceStarter"
 
 let getProjectName: string =
     let getDirectoryPath = Directory.GetCurrentDirectory()
-
     let splitedPath = Directory.GetFiles(getDirectoryPath, "*csproj").[0].Split('\\', '/')
-
     splitedPath.[splitedPath.Length - 1].Replace(".csproj", "")
     
-
 for file in dockerFiles do
+    let linesArray = ResizeArray<string>()
+    
     let mutable project = getProjectName
     let mutable template = replacement
+    
     if file.Contains("yml") || file.Contains("dcproj") then
         project <- project.ToLower().Replace(".", "")
         template <- template.ToLower()
-    let linesArray = ResizeArray<string>()
+    
     for line in File.ReadAllLines(file) do
         if line.Contains(template) then
             line.Replace(template, project) |> linesArray.Add
         else
             linesArray.Add line
+    
     File.WriteAllLines(file, linesArray)
