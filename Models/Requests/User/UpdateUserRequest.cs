35d9459780eb2c02a6ec27f0ae1f0d23217cd092
d.vasilyev <d.vasilyev@alfaleads.net>
2020-10-11 01:47:37 +0300

    Refactor

--- Models/Requests/User/UpdateUserRequest.cs
+++ Models/Requests/User/UpdateUserRequest.cs
@@ -5,11 +5,11 @@ namespace BackendServiceStarter.Models.Requests.User
     public class UpdateUserRequest
     {
         [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; }
         
-        public string? Password { get; set; }
+        public string Password { get; set; }
         
-        public string? ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
         
         public UserRole? Role { get; set; }
     }
